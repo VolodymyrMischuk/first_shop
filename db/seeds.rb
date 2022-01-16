33eb5e1ec35e34cf4b16a9892bb02a33d900ad0d
Volodymyr Mischuk <volodymyr.mischuk.mail@gmail.com>
2022-01-16 12:13:55 +0200

    My first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all
+# . . .
+Product.create(title: 'Progrming Ruby 1.9',
+               description:
+                 %{<p>
+                    jghub fjy jf ytfjt ghfhgvj ffjhgdhgn ytffgthd jgjvj ffjfghcf
+                    dhgdhgcfh dhfdhghgf hdhghd dhd jgf   jfj gfj ghdhdgfsgfhgdcghcfg
+                    dtdhgh  hgchgdg  gh gff  ffgfj  gf f f f g gff
+                 </p>},
+               image_url: 'ruby.jpg',
+               price: 49.95)
+# . . .
